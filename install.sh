3a802d05dfdd724fd84a93daf1650817fcc7e610
Adam Sanderson <adam@adamsanderson.co.uk>
2024-11-15 14:07:26 +0000

    clone omz plugins

--- install.sh
+++ install.sh
@@ -34,6 +34,20 @@ ln -sf ~/dotfiles/.oh-my-zsh/custom ~/.oh-my-zsh/custom
 # Set Zsh as default shell
 chsh -s $(which zsh)
 
-git clone https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+if [! -d "~/.oh-my-zsh/custom/plugins/jq"] then
+  git clone https://github.com/reegnz/jq-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/jq
+fi
+if [! -d "~/.oh-my-zsh/custom/plugins/zsh-autocomplete"] then
+  git clone https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+fi
+if [! -d "~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"] then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+if [! -d "~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"] then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+if [! -d "~/.oh-my-zsh/custom/themes/powerlevel10k"] then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
 
 echo "Installation complete! Please restart your terminal."
